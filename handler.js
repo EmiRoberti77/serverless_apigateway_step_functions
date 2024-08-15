32c64b2dab8ace84895381321233b5389a4a2aa4
EmiRoberti77 <emi@datamotion.ai>
2024-08-15 07:31:44 +0100

    created AWS state engine to be triggered by the API gateway

---
+++ handler.js
@@ -0,0 +1,9 @@
+exports.start = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v4.0! Your function executed successfully!",
+    }),
+    event,
+  };
+};
